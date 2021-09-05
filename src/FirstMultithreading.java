9383e144f00eed34ea7316b90fee602f413e8b74
Oleksii Feshchenko <feshchenkooleksiistudy@gmail.com>
2021-09-05 19:52:01 +0200

    first mult

---
+++ src/FirstMultithreading.java
@@ -0,0 +1,45 @@
+import java.util.SortedMap;
+
+public class FirstMultithreading {
+    public static void main(String[] args) throws InterruptedException{
+        MyThread myThread = new MyThread();
+        myThread.start();
+
+//        for (int i = 0; i < 1000; i++) {
+//            System.out.println("Hello from main Thread");
+//        }
+        MyThread myThread2 = new MyThread();
+        myThread2.start();
+
+        Thread thread = new Thread(new Runner());
+        thread.start();
+    }
+}
+
+class Runner implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Hello from MyThread" + i + "Hi");
+        }
+    }
+}
+
+class MyThread extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Hello from MyThread" + i);
+        }
+    }
+}
